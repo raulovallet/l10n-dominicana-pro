7bdc3de7fade0a8eab035c195db6cfffa8be0bf7
Raul Ovalle <raulovallet@gmail.com>
2023-08-29 22:50:26 -0400

    [ADD] l10n_do_pos: start credit not payment

--- l10n_do_pos/static/src/js/models.js
+++ l10n_do_pos/static/src/js/models.js
@@ -1,7 +1,7 @@
 odoo.define('l10n_do_pos.models', function (require) {
     "use strict";
 
-    var { Order, PosGlobalState } = require('point_of_sale.models');
+    var { Order, PosGlobalState, Payment } = require('point_of_sale.models');
     var Registries = require('point_of_sale.Registries');
 
     const L10nDoPosPosGlobalState = PosGlobalState => class extends PosGlobalState {
@@ -71,6 +71,16 @@ odoo.define('l10n_do_pos.models', function (require) {
             );
 
             return credit_note_payment_method;
+        }
+        async get_credit_note(ncf) {
+            return this.env.services.rpc({
+                model: 'pos.order',
+                method: 'get_credit_note',
+                args: [
+                    false,
+                    ncf
+                ],
+            });
         }
     }
 
@@ -169,136 +179,38 @@ odoo.define('l10n_do_pos.models', function (require) {
             this.ncf_origin_out = ncf_origin_out;
         }
 
-        // TODO: Try for return order
-        // For returned order (credit note)
-        // add_payment_credit_note(credit_note_ncf, cashregister) {
-        //     var self = this;
-        //     var payment_lines = self.get_paymentlines();
-        //     var is_on_payment_line = false;
+    }
+    const L10nDoPayment = Payment => class extends Payment {
+        /**
+         * @override
+         */
+        constructor(obj, options) {
+            super(...arguments); 
 
-        //     payment_lines.forEach(
-        //         function (payment_line) {
-        //             if (payment_line.get_returned_ncf()) {
-        //                 if (payment_line.get_returned_ncf() ===
-        //                     credit_note_ncf) {
-        //                     is_on_payment_line = true;
-        //                 }
-        //             }
-        //         });
-
-        //     if (is_on_payment_line) {
-
-        //         self.pos.gui.show_popup('error', {
-        //             'title': _t('The credit note is in the order'),
-        //             'body': _t('Credit note ' + credit_note_ncf +
-        //                 ' is in the order, please try again'),
-        //         });
-
-        //         return false;
-
-        //     }
-        //     // TODO: esta parte podria buscar mejor por la factura y
-        //     //  no por la orden
-        //     var domain = [
-        //         ['ncf', '=', credit_note_ncf],
-        //         ['returned_order', '=', true],
-        //         ['is_used_in_order', '=', false],
-        //     ];
-        //     self.pos.loading_screen_on();
-        //     rpc.query({
-        //         model: 'pos.order',
-        //         method: 'search_read',
-        //         args: [domain],
-        //         limit: 1,
-        //     } {
-        //         timeout: 3000,
-        //         shadow: true, 
-        //     }).then(function (result) {
-
-        //         if (result.length > 0) {
-        //             self.add_paymentline(cashregister);
-        //             var select_paymentline = self.selected_paymentline;
-        //             select_paymentline.set_returned_ncf(credit_note_ncf);
-        //             select_paymentline.set_returned_order_amount(
-        //                 -1 * result[0].amount_total
-        //             );
-        //             select_paymentline.set_amount(
-        //                 -1 * result[0].amount_total
-        //             );
-        //             self.pos.gui.screen_instances
-        //                 .payment.reset_input();
-        //             self.pos.gui.screen_instances
-        //                 .payment.render_paymentlines();
-        //             self.pos.loading_screen_off();
-
-        //         } else {
-        //             self.pos.loading_screen_off();
-        //             self.pos.gui.show_popup('error', {
-        //                 'title': _t('Not exist'),
-        //                 'body': _t('Credit mote number ' + credit_note_ncf +
-        //                     ' does exist'),
-        //             });
-        //         }
-        //     } function (err, ev) {
-        //         self.pos.loading_screen_off();
-        //         console.log(err);
-        //         console.log(ev);
-        //         ev.preventDefault();
-        //         var error_body =
-        //             _t('Your Internet connection is probably down.');
-        //         if (err.data) {
-        //             var except = err.data;
-        //             error_body = except.arguments ||
-        //                 except.message || error_body;
-        //         }
-        //         self.gui.show_popup('error', {
-        //             'title': _t('Error: Could not Save Changes'),
-        //             'body': error_body,
-        //         });
-        //     });
-        // }
+            this.credit_note_ncf = this.credit_note_ncf || '';
+            this.credit_note_partner_id = this.credit_note_partner_id || false;
+        }
+        //@override
+        export_as_JSON() {
+            const json = super.export_as_JSON(...arguments);
+            json.credit_note_ncf = this.credit_note_ncf;
+            json.credit_note_partner_id = this.credit_note_partner_id;
+            return json;
+        }
+        //@override
+        init_from_JSON(json) {
+            super.init_from_JSON(...arguments);
+            this.credit_note_ncf = json.credit_note_ncf;
+            this.credit_note_partner_id = json.credit_note_partner_id;
+        }
+        set_fiscal_data(ncf, partner_id){
+            this.credit_note_ncf = ncf;
+            this.credit_note_partner_id = partner_id;
+        }
     }
 
-    // var models = require('point_of_sale.models');
-    // var core = require('web.core');
-    // var _t = core._t;
-    // var rpc = require('web.rpc');
-    // var _super_order = models.Order.prototype;
-
-    // models.load_fields('res.partner', ['sale_fiscal_type_id']);
-    // models.load_fields('account.journal', ['is_for_credit_notes']);
-    // models.load_fields('account.tax', ['tax_group_id']);
-
-
-    // var _paylineproto = models.Paymentline.prototype;
-    // models.Paymentline = models.Paymentline.extend({
-    //     initialize() {
-    //         _paylineproto.initialize.apply(this, arguments);
-    //         this.returned_ncf = null;
-    //         this.returned_order_amount = 0;
-    //     }
-
-    //     set_returned_ncf(returned_move_name) {
-    //         this.returned_ncf = returned_move_name;
-    //     }
-    //     get_returned_ncf() {
-    //         return this.returned_ncf;
-    //     }
-    //     set_returned_order_amount(returned_order_amount) {
-    //         this.returned_order_amount = returned_order_amount;
-    //     }
-    //     get_returned_order_amount() {
-    //         return this.returned_order_amount;
-    //     }
-    //     export_as_JSON() {
-    //         var loaded = _paylineproto.export_as_JSON.call(this);
-    //         loaded.returned_ncf = this.get_returned_ncf();
-    //         return loaded;
-    //     }
-    // });
-
-    // return models;
     Registries.Model.extend(PosGlobalState, L10nDoPosPosGlobalState);
     Registries.Model.extend(Order, L10nDoPosOrder);
+    Registries.Model.extend(Payment, L10nDoPayment);
 
 });
