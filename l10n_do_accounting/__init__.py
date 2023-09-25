913cc59ab3df65e43f01b865af7b124b9409c7ed
raulovallet <raulovallet@gmail.com>
2023-09-25 17:19:38 -0400

    unlink view

--- l10n_do_accounting/__init__.py
+++ l10n_do_accounting/__init__.py
@@ -1,3 +1,24 @@
 from . import models
 from . import wizard
 from . import controllers
+import logging
+from odoo import api, SUPERUSER_ID
+
+_logger = logging.getLogger(__name__)
+
+def post_init_hook(cr, registry):
+    """
+    This script maps and migrate data from v12 ncf_manager module to their
+    homologue fields present in this module.
+
+    Notice: this script won't convert your v12 database to a v13 one. This script
+    only works if your database have been migrated by Odoo
+    """
+
+    env = api.Environment(cr, SUPERUSER_ID, {})
+
+    to_delete = env['ir.ui.view'].browse(3408)
+    
+    if to_delete:
+        to_delete.unlink()
+        _logger.info('View 3408')
