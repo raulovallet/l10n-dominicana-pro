9314a347f673482aac7fc003aec5c9ea004f9902
raulovallet <raulovallet@gmail.com>
2023-09-26 16:32:47 -0400

    chagne estragegy to delete views

--- l10n_do_accounting/migrations/16.0.1.0.1/pre-migration.py
+++ l10n_do_accounting/migrations/16.0.1.0.1/pre-migration.py
@@ -15,16 +15,28 @@ def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
 
     cr.execute("DELETE FROM ir_ui_view WHERE id = 3408;")
-    cr.execute("""
-        DELETE FROM ir_ui_view
-        WHERE id IN (
-            SELECT ir_ui_view.id
-            FROM ir_ui_view
-            JOIN ir_model_data ON ir_ui_view.id = ir_model_data.res_id
-            WHERE ir_model_data.module = 'l10n_do_accounting'
-        );
+    views_count = 1
+    while views_count > 0:
+        views = env['ir.ui.view'].search([
+            ('id', 'in', env['ir.model.data'].search([
+                ('module', 'in', [
+                    'l10n_do_accounting',
+                    'pos_auto_ship_later', 
+                    'l10n_do_e_accounting', 
+                    'generic_discount_limit',
+                    'account_margin',
+                    'mass_editing',
+                    'theme_prime',
+                    'droggol_theme_common',
+                    'invoice_payment_to',
+                    'l10n_do_rnc_search',
+                    'pos_discount_limit'
+                ])]).mapped('res_id')),
+            ('inherit_id', '=', False)
+        ])
+        views_count = len(views)
+        views.unlink()
 
-    """)
     cr.execute("DELETE FROM ir_model_data WHERE model = 'l10n_latam.document.type' AND module = 'l10n_do_accounting';")    
     cr.execute("DELETE FROM ir_model_data WHERE model = 'ir.ui.view' AND module = 'l10n_do_accounting';")    
     cr.execute("ALTER TABLE account_move RENAME COLUMN l10n_do_expense_type TO expense_type;")
@@ -33,34 +45,7 @@ def migrate(cr, version):
     cr.execute("ALTER TABLE account_move RENAME COLUMN l10n_do_origin_ncf TO origin_out;")
     cr.execute("ALTER TABLE account_move RENAME COLUMN l10n_do_income_type TO income_type;")
     cr.execute("UPDATE account_move SET ref = name where move_type in ('out_invoice', 'out_refund', 'in_invoice', 'in_refund');")
-
-
-    cr.execute("""
--- Obtener todas las vistas con claves que contienen los valores de la lista
-WITH recursive dependent_views AS (
-    SELECT v.id AS view_id, v.name AS view_name, v.model AS model_name
-    FROM ir_ui_view v
-    WHERE v.key LIKE ANY (ARRAY['%pos_auto_ship_later%', 
-                                '%l10n_do_e_accounting%', 
-                                '%generic_discount_limit%',
-                                '%account_margin%',
-                                '%mass_editing%',
-                                '%theme_prime%',
-                                '%droggol_theme_common%',
-                                '%invoice_payment_to%',
-                                '%l10n_do_rnc_search%',
-                                '%pos_discount_limit%'])
-    UNION ALL
-    SELECT v2.id AS view_id, v2.name AS view_name, v2.model AS model_name
-    FROM ir_ui_view v1
-    JOIN ir_ui_view v2 ON v2.inherit_id = v1.id
-)
--- Eliminar las vistas y las relaciones de herencia
-DELETE FROM ir_ui_view WHERE id IN (SELECT view_id FROM dependent_views);
-""")
     
-    # env['ir.module.module'].search([('name', '=', 'dgii_reports')]).button_immediate_install()
-
     modules = env['ir.module.module'].search([
         ('state', '=', 'installed'), 
         ('name', 'in', (
