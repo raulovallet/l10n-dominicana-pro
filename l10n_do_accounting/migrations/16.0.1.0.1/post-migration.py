22f54ffab920ff5ad69bf80201995ed46ac856be
Raul Ovalle <raulovallet@gmail.com>
2023-10-02 00:18:14 -0400

    add partners

--- l10n_do_accounting/migrations/16.0.1.0.1/post-migration.py
+++ l10n_do_accounting/migrations/16.0.1.0.1/post-migration.py
@@ -10,6 +10,7 @@ def migrate(cr, version):
     account_moves = env['account.move'].search([
         ('move_type', 'in', ('out_invoice', 'out_refund', 'in_invoice', 'in_refund'))
     ])
+    partners = env['res.partner'].search([])
     
     for account_move in account_moves:
         fiscal_type_id = env['account.fiscal.type'].search([
@@ -20,6 +21,7 @@ def migrate(cr, version):
             'fiscal_type_id': fiscal_type_id.id if fiscal_type_id else False
         })
     
-    
     account_moves._compute_is_l10n_do_fiscal_invoice()
+    partners._compute_sale_fiscal_type_id()
+    
     _logger.info('############## POS script executed successfully l10n_do_accounting ##############')
