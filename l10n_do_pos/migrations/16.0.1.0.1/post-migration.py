f4f0b2a319112897a4557e8872e310d97b5fa505
Raul Ovalle <raulovallet@gmail.com>
2023-10-02 08:14:05 -0400

    update script

--- l10n_do_pos/migrations/16.0.1.0.1/post-migration.py
+++ l10n_do_pos/migrations/16.0.1.0.1/post-migration.py
@@ -7,15 +7,16 @@ _logger = logging.getLogger(__name__)
 def migrate(cr, version):
     
     env = api.Environment(cr, SUPERUSER_ID, {})
-    pos_orders = env['pos.order'].search([])
+    # Maybe not necessary
+    # pos_orders = env['pos.order'].search([])
     
-    for pos_order in pos_orders:
-        fiscal_type_id = env['account.fiscal.type'].search([
-            ('prefix', '=', pos_order.l10n_latam_document_type_id.doc_code_prefix),
-            ('type', 'in', ('out_invoice', 'out_refund'))
-        ], limit=1)
-        pos_order.write({
-            'fiscal_type_id': fiscal_type_id.id if fiscal_type_id else False
-        })
+    # for pos_order in pos_orders:
+    #     fiscal_type_id = env['account.fiscal.type'].search([
+    #         ('prefix', '=', pos_order.l10n_latam_document_type_id.doc_code_prefix),
+    #         ('type', 'in', ('out_invoice', 'out_refund'))
+    #     ], limit=1)
+    #     pos_order.write({
+    #         'fiscal_type_id': fiscal_type_id.id if fiscal_type_id else False
+    #     })
 
     _logger.info('############## Pre script executed successfully l10n_do_accounting views deleted. ##############')
