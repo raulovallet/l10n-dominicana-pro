02138ccb015c14f00ba9cc366f94f41f78f1bf79
Raul Ovalle <raulovallet@gmail.com>
2023-11-24 01:53:18 -0500

    [ADD] l10n_do_pos: better logic for create contact

--- l10n_do_pos/__manifest__.py
+++ l10n_do_pos/__manifest__.py
@@ -5,7 +5,7 @@
     'license': 'LGPL-3',
     'website': "https://github.com/odoo-dominicana",
     'category': 'Localization',
-    'version': '16.0.2.0.6',
+    'version': '16.0.2.0.7',
     'depends': [
         'base',
         'point_of_sale',
