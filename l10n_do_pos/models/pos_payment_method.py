6cfd59d30c7fe1a62cfed571aa656fb7d096d1f8
raulovallet <raulovallet@gmail.com>
2023-12-22 12:52:03 -0500

    [FIX] l10n_do_pos: fix point of sale

--- l10n_do_pos/models/pos_payment_method.py
+++ l10n_do_pos/models/pos_payment_method.py
@@ -8,4 +8,14 @@ class PosPaymentMethod(models.Model):
         string='Credit Note',
     )
 
-    # TODO: check if split_transactions is true and journal_id is should be a false
+    @api.constrains('is_credit_note')
+    def _check_is_credit_note(self):
+        for record in self:
+            if record.is_credit_note:
+                if not record.split_transactions:
+                    raise models.ValidationError(
+                        _('Identify customer must be true if is credit note is true.'))
+
+                if record.journal_id:
+                    raise models.ValidationError(
+                        _('Journal must be empty if is credit note is true.'))
