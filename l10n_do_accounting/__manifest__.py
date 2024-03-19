4dafd3fe492ee4af677b76e84296494b53bab16e
raulovallet <raulovallet@gmail.com>
2024-03-19 15:18:36 -0400

    [FIX] l10n_do_account: Update version number and remove unused code

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -9,7 +9,7 @@
     "license": "LGPL-3",
     "website": "https://github.com/odoo-dominicana",
     "category": "Localization",
-    "version": "16.0.2.0.6",
+    "version": "16.0.2.0.7",
     # any module necessary for this one to work correctly
     "depends": [
         "base",
