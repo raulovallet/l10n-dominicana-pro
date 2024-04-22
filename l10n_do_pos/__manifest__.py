b619ef276baa77400c6a5db5aea687bc3d9ed5f7
raulovallet <raulovallet@gmail.com>
2024-04-22 13:47:22 -0400

    [BUMP] l10n_do_pos: Update version number to 16.0.2.1.4

--- l10n_do_pos/__manifest__.py
+++ l10n_do_pos/__manifest__.py
@@ -5,7 +5,7 @@
     'license': 'LGPL-3',
     'website': "https://github.com/odoo-dominicana",
     'category': 'Localization',
-    'version': '16.0.2.1.3',
+    'version': '16.0.2.1.4',
     'depends': [
         'base',
         'point_of_sale',
