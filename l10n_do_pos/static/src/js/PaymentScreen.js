1127b820a08f3cc41e8c6961f72680cfe12a5384
raulovallet <raulovallet@gmail.com>
2024-11-04 11:29:49 -0500

    [FIX] l10n_do_pos: payment

--- l10n_do_pos/static/src/js/PaymentScreen.js
+++ l10n_do_pos/static/src/js/PaymentScreen.js
@@ -261,6 +261,7 @@ odoo.define('l10n_do_pos.PaymentScreen', function (require) {
 
                 var current_order = this.env.pos.get_order();
                 var total_in_bank = 0;
+                var total_in_pay_later = 0;
                 var has_cash = false;
                 var payment_lines = current_order.get_paymentlines();
                 var total = current_order.get_total_with_tax();
@@ -271,6 +272,10 @@ odoo.define('l10n_do_pos.PaymentScreen', function (require) {
                 for (let payment_line of payment_lines) {
                     if (payment_line.payment_method.type === 'bank') {
                         total_in_bank = +Number(payment_line.amount);
+                    }
+
+                    if(payment_line.payment_method.type === 'pay_later' && !payment_line.payment_method.is_credit_note){
+                        total_in_pay_later = +Number(payment_line.amount);
                     }
 
                     if (payment_line.payment_method.type === 'cash') {
@@ -304,6 +309,7 @@ odoo.define('l10n_do_pos.PaymentScreen', function (require) {
                                 title: _t('Error in credit note'),
                                 body: _t('The credit note has no residual amount, please delete the payment of the credit note and enter it again.'),
                             });
+                            return false;
                         }
 
                         if (credit_note.residual_amount < payment_line.amount) {
@@ -312,6 +318,7 @@ odoo.define('l10n_do_pos.PaymentScreen', function (require) {
                                 body: _t(
                                     'The amount of the credit note is less than the amount entered, please delete the payment of the credit note and enter it again.'),
                             });
+                            return false;
                         }
 
                         // TODO: Check if this is necessary
@@ -338,6 +345,28 @@ odoo.define('l10n_do_pos.PaymentScreen', function (require) {
 
                     return false;
                 }
+                
+                if (Math.abs(Math.round(Math.abs(total) * 100) / 100) <
+                    Math.round(Math.abs(total_in_pay_later) * 100) / 100) {
+
+                    this.showPopup('ErrorPopup', {
+                        title: _t('Pay later payment'),
+                        body: _t('Pay later payment cannot exceed the total order'),
+                    });
+
+                    return false;
+                }
+
+                if (Math.abs(Math.round(Math.abs(total) * 100) / 100) <
+                    Math.round(Math.abs(total_in_pay_later+total_in_bank) * 100) / 100) {
+
+                    this.showPopup('ErrorPopup', {
+                        title: _t('Card and pay later payment'),
+                        body: _t('The sum for Card and Pay Later payment cannot exceed the total order.'),
+                    });
+
+                    return false;
+                }
 
                 if (Math.round(Math.abs(total_in_bank) * 100) / 100 ===
                     Math.round(Math.abs(total) * 100) / 100 && has_cash) {
@@ -353,6 +382,21 @@ odoo.define('l10n_do_pos.PaymentScreen', function (require) {
                     return false;
                 }
 
+
+                if (Math.round(Math.abs(total_in_pay_later) * 100) / 100 ===
+                    Math.round(Math.abs(total) * 100) / 100 && total_in_pay_later && has_cash) {
+
+                    this.showPopup('ErrorPopup', {
+                        title: _t('Pay later and cash payment'),
+                        body: _t('The total payment with the pay later is ' +
+                            'sufficient to pay the order, please eliminate the ' +
+                            'payment in cash or reduce the amount to be paid by ' +
+                            'pay later'),
+                    });
+
+                    return false;
+                }
+                
                 
                 // TODO: Check if this is necessary
                 // if (!payment_and_return_mount_equals) {
