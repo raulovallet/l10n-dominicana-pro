c80ac8189f0c1f2149413727cead4c3c72699baa
stferraro <gerferr83@gmail.com>
2025-01-07 13:24:42 -0400

    [FIX] cannot delete pos_partners

--- l10n_do_pos/models/res_partner.py
+++ l10n_do_pos/models/res_partner.py
@@ -7,18 +7,7 @@ from odoo.exceptions import UserError, ValidationError
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     
-    def _get_res_partner_list(self):
-        
-        POS_CONSUMER_PARTNER = self.env.ref('l10n_do_pos.default_pos_partner')
-        DEFAULT_PARTNER = self.env.ref('l10n_do_pos.default_pos_partner')
-        
-        
-        return {
-            'pos_consumer_partner': POS_CONSUMER_PARTNER.id,
-            'default_pos_partner': DEFAULT_PARTNER.id
-        }
-
     def unlink(self):
-        if self.filtered(lambda x: x.id in self._get_res_partner_list().values()):
+        if self.filtered(lambda x: x.self.env.ref('l10n_do_pos.default_pos_partner').id):
             raise UserError(_("You can't delete this partner."))
         return super(ResPartner, self).unlink()
