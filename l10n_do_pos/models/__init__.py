2fcb5d6e7f004402cf25ab6902ce32080e76577d
stferraro <gerferr83@gmail.com>
2025-01-07 12:27:17 -0400

    [FIX] cannot delete cliente de consumo

--- l10n_do_pos/models/__init__.py
+++ l10n_do_pos/models/__init__.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from . import pos_config
 from . import pos_order
 from . import res_config_settings
 from . import pos_session
 from . import pos_payment_method
 from . import pos_payment
+from . import res_partner
