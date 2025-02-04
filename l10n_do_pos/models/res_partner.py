985615b7d31eed507e31335d039d92e3ac68878d
stferraro <gerferr83@gmail.com>
2025-02-04 11:28:26 -0400

    [FIX] new branch - 16.0_ultimos_cambios_ya

---
+++ l10n_do_pos/models/res_partner.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    
+    def _get_res_partner_list(self):
+        
+        POS_CONSUMER_PARTNER = self.env.ref('l10n_do_pos.default_pos_partner')
+        
+        
+        return {
+            'pos_consumer_partner': POS_CONSUMER_PARTNER.id
+        }
+
+    def unlink(self):
+        if self.filtered(lambda x: x.id in self._get_res_partner_list().values()):
+            raise UserError(_("You can't delete this partner."))
+        return super(ResPartner, self).unlink()
