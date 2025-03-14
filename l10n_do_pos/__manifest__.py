b6874f3c2ed33f6ea68930592ed4be51a36cd1a0
stferraro <gerferr83@gmail.com>
2025-03-14 16:10:20 -0400

    [ADD] cannot delete consumer cliente

--- l10n_do_pos/__manifest__.py
+++ l10n_do_pos/__manifest__.py
@@ -5,7 +5,7 @@
     'license': 'LGPL-3',
     'website': "https://github.com/odoo-dominicana",
     'category': 'Localization',
-    'version': '16.0.2.3.3',
+    'version': '16.0.2.3.4',
     'depends': [
         'base',
         'point_of_sale',
