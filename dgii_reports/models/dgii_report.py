c084033301b75dbcdc04f4310c3b21a062539bd3
stferraro <gerferr83@gmail.com>
2025-03-14 17:30:09 -0400

    [ADD] button copy in dgii reports

--- dgii_reports/models/dgii_report.py
+++ dgii_reports/models/dgii_report.py
@@ -17,6 +17,14 @@ except ImportError:
         _("This module needs pycountry to get 609 ISO 3166 "
           "country codes. Please install pycountry on your system. "
           "(See requirements file)"))
+    
+try :
+    import pyperclip
+except ImportError:
+    raise ImportError(
+        _("This module needs pyperclip to copy data to clipboard. "
+          "Please install pyperclip on your system. "
+          "(See requirements file)"))
           
 
 class DgiiReport(models.Model):
@@ -1996,6 +2004,12 @@ class DgiiReport(models.Model):
                 self.env.ref('dgii_reports.dgii_exterior_report_line_tree').id,
             'domain': [('dgii_report_id', '=', self.id)]
         }
+        
+    def action_copy_to_clipboard(self, field_name):
+        for rec in self:
+            value = getattr(rec, field_name, "")
+            pyperclip.copy(str(value))
+        return True
 
 
 class DgiiReportPurchaseLine(models.Model):
@@ -2216,3 +2230,9 @@ class DgiiReportsIt1(models.Model):
         comodel_name='account.move.line',
         string='Move Lines',
     )
+    
+    def action_copy(self):
+        for rec in self:
+            pyperclip.copy(str(rec.amount))
+        return True
+
