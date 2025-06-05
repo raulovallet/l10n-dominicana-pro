b289be9940b498bb1631f56e21444909f61ab24b
stferraro <gerferr83@gmail.com>
2025-06-05 16:56:10 -0400

    [ADD] doesnt archivate the account type fiscal

--- l10n_do_pos/models/account_fiscal_type.py
+++ l10n_do_pos/models/account_fiscal_type.py
@@ -9,8 +9,7 @@ class AccountFiscalType(models.Model):
     def get_active_sessions(self):
         self.ensure_one()
         return self.env['pos.session'].search([
-            ('state', 'in', ['opening_control', 'opened']),
-            ('config_id.l10n_do_fiscal_type_id', '=', self.id),
+            ('state', '!=', 'closed'),
         ])
 
     def get_pos_config_names(self, pos_sessions):
@@ -24,8 +23,5 @@ class AccountFiscalType(models.Model):
                 pos_sessions = fiscal_type.get_active_sessions()
                 if pos_sessions:
                     config_names = fiscal_type.get_pos_config_names(pos_sessions)
-                    raise ValidationError(_(
-                        'You cannot archive a fiscal type that is used in an open POS session.\n'
-                        'Affected POS: %s'
-                    ) % config_names)
-        return super().write(vals)
+                    raise ValidationError(_('You cannot archive a fiscal type'))
+        return super(AccountFiscalType, self).write(vals)
