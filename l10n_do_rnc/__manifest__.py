e00d996360248c196fce3c2d018e6fc663adedda
stferraro <gerferr83@gmail.com>
2025-09-08 16:01:39 -0400

    [FIX] repair bugs with add data demo

--- l10n_do_rnc/__manifest__.py
+++ l10n_do_rnc/__manifest__.py
@@ -13,7 +13,7 @@
     'website': "https://www.guavana.com",
     'license': 'LGPL-3',
     'category': 'Localization',
-    'version': '16.0.1.1.3',
+    'version': '16.0.1.1.4',
     'depends': [
         'base',
         'contacts',
